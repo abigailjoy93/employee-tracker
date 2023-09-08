0da56ef7c52763625a1a4e510602dfa5ac6414cf
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-09-07 20:14:57 -0500

    fifth commit

--- index.js
+++ index.js
@@ -70,3 +70,52 @@ async function viewEmployees() {
 // CRITERIA: Create a prompt to select an employee to update and then update the db
 
 // TODO: Create a startApplication function that displays the main menu
+function startApplication() {
+  inquirer
+  .prompt ({
+    {
+      type: "list",
+      name: "action",
+      message: "Welcome! What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit",
+      ],
+    },
+  })
+  .then((answer) => {
+    switch (answer.action) {
+      case "View all departments":
+        viewDepartments();
+        break;
+      case "View all roles":
+        viewRoles();
+        break;
+      case "View all employees":
+        viewEmployees();
+        break;
+      case "Add a department":
+        addDepartment();
+        break;
+      case "Add a role":
+        addRole();
+        break;
+      case "Add an employee":
+        addEmployee();
+        break;
+      case "Update an employee role":
+        updateRole();
+        break;
+      case "Exit":
+        connection.end();
+        console.log("Goodbye!");
+        break;
+    }
+  })
+}
