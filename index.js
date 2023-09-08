ede571b83b2c137c184966cf2c2d4014ca745686
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-09-07 19:53:18 -0500

    third commit

--- index.js
+++ index.js
@@ -18,3 +18,34 @@ connection.connect((err) => {
   if (err) throw err;
   startApplication();
 });
+
+// TODO: Create a function to view all departments
+// CRITERIA: Present a formatted table showing department names and ids
+async function viewDepartments() {
+  try {
+    const departments = await queryAsync("SELECT * FROM department");
+    console.table(departments);
+    startApplication();
+  } catch (err) {
+    console.error("Error viewing departments:", err);
+    startApplication();
+  }
+}
+
+// TODO: Create a function to view all roles
+// CRITERIA: List job title, role id, department, salary
+
+// TODO: Create a function to view all employees
+// CRITERIA: Present a formatted table showing employee id, first name, last name, job title, department, salary, manager
+
+// TODO: Create a function to add a department
+// CRITERIA: Create a prompt to enter the name of the department and have that department added to the DB
+
+// TODO: Create a function to add a role
+// CRITERIA: Create a prompt to enter the name, salary, and department and add it to the db
+
+// TODO: Create a function to add an employee
+// CRITERIA: Create a prompt to enter the first name, last name, role, manager and add them to the db
+
+// TODO: Create a function to update an employee role
+// CRITERIA: Create a prompt to select an employee to update and then update the db
